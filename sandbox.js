532d1773f557295f57b781cda3fb6ce69f95acf0
Hatim Patrawala <hatim.patrawala@livehealth.in>
2022-01-09 21:32:39 +0530

    Learning: Tsc compiler and watch mode

---
+++ sandbox.js
@@ -0,0 +1,4 @@
+var character = 'Mario';
+console.log(character);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
---
+++ sandbox.ts
@@ -0,0 +1,8 @@
+const character = 'Mario';
+
+console.log(character);
+
+const inputs = document.querySelectorAll('input')
+
+console.log(inputs);
+
